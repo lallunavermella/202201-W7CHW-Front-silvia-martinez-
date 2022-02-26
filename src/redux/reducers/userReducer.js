da0f4222724602bd15876632541f5345666364c4
Silvia Martinez <sil@silvia.com>
2022-02-26 20:15:57 +0100

    Update reducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,14 +1,14 @@
 import actionTypes from "../actions/actionTypes";
 
-const userReducer = (currentUsers = [], actions = {}) => {
+const userReducer = (currentUsers = {}, actions = {}) => {
   let newUser;
   switch (actions.type) {
     case actionTypes.registerUSer:
-      newUser = [...currentUsers, actions.user];
+      newUser = { ...actions.user };
       break;
 
     default:
-      newUser = [...currentUsers];
+      newUser = { ...currentUsers };
       break;
   }
   return newUser;
