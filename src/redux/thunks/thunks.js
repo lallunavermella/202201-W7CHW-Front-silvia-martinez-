1afb35a198ddc2e4d8837f59ab5b7ee3ecf69dc2
Silvia Martinez <sil@silvia.com>
2022-02-26 20:16:37 +0100

    Update reducer, fails connection api

--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -1,23 +1,16 @@
 import { userRegisterAction } from "../actions/actionCreators";
 
-export const registerUserThunk =
-  ({ name, image, username, password }) =>
-  async (dispatch) => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        image,
-        username,
-        password,
-      }),
-    });
-    const newUser = await response.json();
+export const registerUserThunk = (user) => async (dispatch) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  const newUser = await response.json();
 
-    if (response.ok) {
-      dispatch(userRegisterAction(newUser));
-    }
-  };
+  if (response.ok) {
+    dispatch(userRegisterAction(newUser));
+  }
+};
