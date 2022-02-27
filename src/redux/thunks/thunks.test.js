1434f03b2898687dae577deda6a6bdf7e5eb510a
Silvia Martinez <sil@silvia.com>
2022-02-27 16:45:25 +0100

    Added test to thunk login

--- src/redux/thunks/thunks.test.js
+++ src/redux/thunks/thunks.test.js
@@ -1,7 +1,11 @@
-import { registerUserThunk } from "./thunks";
+import { loginUserThunk, registerUserThunk } from "./thunks";
+
+import jwtDecode from "jwt-decode";
+
+jest.mock("jwt-decode");
 
 describe("Given a userRegisterThunk inner function", () => {
-  describe("When it is called with id 1", () => {
+  describe("When it is called with a new user", () => {
     test("Then it should call the dispatch", async () => {
       const dispatch = jest.fn();
       const user = {
@@ -11,6 +15,27 @@ describe("Given a userRegisterThunk inner function", () => {
       };
 
       const registerThunk = registerUserThunk(user);
+
+      await registerThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given loginUserrThunk inner function", () => {
+  describe("When it is called with a user", () => {
+    test("Then it should call the dispatch", async () => {
+      const dispatch = jest.fn();
+      jwtDecode.mockReturnValueOnce({ id: 1, username: "pepe" });
+
+      const user = {
+        name: "pepe",
+        username: "pepito",
+        password: "123",
+      };
+
+      const registerThunk = loginUserThunk(user);
 
       await registerThunk(dispatch);
 
