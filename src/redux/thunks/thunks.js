6b4193f95d8bddca43664d7de80929d28dc1637e
Silvia Martinez <sil@silvia.com>
2022-02-27 14:31:41 +0100

    Added loginThunk

--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -1,4 +1,5 @@
-import { userRegisterAction } from "../actions/actionCreators";
+import { loginUserAction, userRegisterAction } from "../actions/actionCreators";
+import jwtDecode from "jwt-decode";
 
 export const registerUserThunk = (user) => async (dispatch) => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}register`, {
@@ -8,6 +9,24 @@ export const registerUserThunk = (user) => async (dispatch) => {
     },
     body: JSON.stringify(user),
   });
+
   const newUser = await response.json();
   dispatch(userRegisterAction(newUser));
+};
+
+export const loginUserThunk = (user) => async (dispatch) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    const token = await response.json();
+    const { id, username } = await jwtDecode(token.token);
+    localStorage.setItem("UserToken", token.token);
+    dispatch(loginUserAction({ id, username, token: token.token }));
+  }
 };
