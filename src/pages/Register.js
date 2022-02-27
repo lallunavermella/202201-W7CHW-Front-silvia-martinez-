338149f4b1b1acac6d3352ac5a0111934c24b8c7
Silvia Martinez <sil@silvia.com>
2022-02-27 11:12:22 +0100

    Created pages/Register

---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar/NavBar";
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const Register = () => {
+  return (
+    <>
+      <NavBar />
+      <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
